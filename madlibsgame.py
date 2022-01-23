6b6a46822845961db58b9f51fe37e21d84aabeda
Sarah <97861666+sarziee@users.noreply.github.com>
2022-01-23 23:48:25 +0800

    Add files via upload

---
+++ madlibsgame.py
@@ -0,0 +1,21 @@
+from xxlimited import foo
+
+
+adjective = input("Enter an adjective: ")
+nationality = input("Enter a nationality: ")
+person = input("Enter a person's name: ")
+noun = input("Enter a noun: ")
+pluralnoun = input("Enter a plural noun: ")
+number = input("Enter a number: ")
+food = input("Enter a food: ")
+food1 = input("Enter another food: ")
+shapes = input("Enter a shape: ")
+
+print("Pizza was invented by a " + nationality + " chef named " + person)
+print("To make a pizza, you need to take a lump of " + noun + " and make a thin, round " + adjective  +  noun)
+print("Then you cover it with " + adjective + " sauce, " + adjective + " cheese, and fresh chopped " +  pluralnoun)
+print("Next you have to bake it in a very hot " + noun)
+print("When it is done, cut it into " + shapes)
+print("Some kids like " + food  + " pizza the best, but my favorite is the " + food + " pizza.")
+print("If I could, I would eat pizza " + number + " times a day!")
+
